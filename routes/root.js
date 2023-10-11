4826a8bb612713b196ecd9e70d4f66113354f961
yusufroqib <yusufroqib@gmail.com>
2023-10-11 11:11:47 +0100

    update

--- routes/root.js
+++ routes/root.js
@@ -7,12 +7,4 @@ router.get('^/$|/index(.html)?', (req, res) => {                        //Makes 
     res.sendFile(path.join(__dirname, '..', "views", "index.html"))
 })
 
-router.get('/new-page(.html)?', (req, res) => {                            //Makes it dynamic to either use .html or not in our address
-    res.sendFile(path.join(__dirname, '..', "views", "new-page.html"))
-})
-
-router.get('/testing(.html)?', (req, res) => {
-    res.redirect(301, "new-page.html")
-})
-
 module.exports = router
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const {logger} = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler')
-const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3000;
 
 // const {router} = require('./routes/subdir')
@@ -13,7 +13,6 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 //Static Routes
-
 app.use('/', express.static(path.join(__dirname, "public")));               //Apply static files
 
  
@@ -25,86 +24,6 @@ app.use(logger)
 
 app.use(cors(corsOptions))
 
-// app.get('/', (req, res) => {
-//     res.sendFile('/views/index.html', {root: __dirname})
-// })                   
-
-            //OR
-
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, "views", "index.html"))
-// })
-
-             //OR
-
-// app.get('^/$|/index.html', (req, res) => {
-//     res.sendFile(path.join(__dirname, "views", "index.html"))
-// })
-            //OR
-            
-// app.get('^/$|/index(.html)?', (req, res) => {                        //Makes it dynamic to either use .html or not in our address
-//     res.sendFile(path.join(__dirname, "views", "index.html"))
-// })
-
-// // app.get('/new-page', (req, res) => {
-// //     res.sendFile('/views/new-page.html', {root: __dirname})
-// // })
-
-//             //OR
-
-// app.get('/new-page(.html)?', (req, res) => {                            //Makes it dynamic to either use .html or not in our address
-//     res.sendFile('/views/new-page.html', {root: __dirname})
-// })
-
-// // app.get('/testing', (req, res) => {
-// //     res.sendFile(path.join(__dirname, 'views', 'testing.html'))
-// // })
-
-
-// //   ********************************************//Redirecting***********************************************
-
-// // app.get('/old-page(.html)?', (req, res) => {
-// //     res.redirect(path.join(__dirname, 'views', 'new-page.html'))
-// // })
-
-//             //OR
-// // app.get('/testing(.html)?', (req, res) => {
-// //     res.redirect(path.join(__dirname, "views", "new-page.html"))
-// // })
-
-//             //OR
-// app.get('/testing(.html)?', (req, res) => {
-//     res.redirect(301, "new-page.html")
-// })
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, "new-page.html")
-})
-
-
-
-// // ***********************Chaining Route Handler**********************************
-// const cohort1 = (req, res, next) => {
-//     console.log('Kanas Qodri');
-//     next()
-// }
-// const cohort2 = (req, res, next) => {
-//     console.log('Muhammad Rocco');
-//     next()
-// }
-// const cohort3 = (req, res, next) => {
-//     console.log('Muhammad KennyMax');
-//     next()
-// }
-// const cohort4 = (req, res) => {
-//     console.log('Supreme HaliahFather');
-//     res.send('Dem be guru in Tech')
-// }
-
-// app.get('/big-devs(.html)?', [cohort1, cohort2, cohort3, cohort4])
-
-// app.get('/*', (req, res) => {                                   //Error 404 page
-//     res.sendFile(path.join(__dirname, 'views', '404.html'))
-// })
 
 app.all('*', (req, res) => {
     res.status(404);
