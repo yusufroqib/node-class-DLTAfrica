7cd8a1788df267b8897018396bc5bc4523b36e7e
yusufroqib <yusufroqib@gmail.com>
2023-10-12 10:59:57 +0100

    update

---
+++ controllers/authController.js
@@ -0,0 +1,25 @@
+const usersDB = {
+    users: require("../model/user.json"),
+    setUsers: function (data) { this.users = data }
+};
+
+const bcrypt = require("bcrypt");
+
+const handleLogin = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({"message": "Invalid username or password"})
+    const foundUser = usersDB.users.find(person => person.username  === user)
+    if(!foundUser) return res.sendStatus(401) //unauthorize
+    //evaluate password
+
+    const match = await compare(pwd, foundUser.password)
+        if(match) {
+            res.json({"success" : `user ${user} is logged in!`})
+        } else {
+            res.sendStatus(401)
+        }
+
+}
+
+
+module.exports = { handleLogin }
