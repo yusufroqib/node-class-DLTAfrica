dcd7e5165a2ea7f23f892bf18f22ae6127131af9
yusufroqib <yusufroqib@gmail.com>
2023-10-13 13:25:24 +0100

    Update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
-app.use(cookieParser)
+app.use(cookieParser())
 
 //Static Routes
 app.use('/', express.static(path.join(__dirname, "public")));               //Apply static files
